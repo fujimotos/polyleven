d7d88e17973eca347e99dac6798821927aa94ea5
Fujimoto Seiji <fujimoto@ceptord.net>
2017-12-08 00:13:32 +0900

    setup.py: update the version info (v0.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name = 'polyleven',
-    version = '1.0',
+    version = '0.1',
     ext_modules = [
         Extension('polyleven', sources=['polyleven.c'])
     ]
