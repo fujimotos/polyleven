4405ea756578ed557e94a73888c0018de53d5bd2
Fujimoto Seiji <fujimoto@ceptord.net>
2018-07-13 01:21:20 +0900

    Add a method "_debug" to call low-level functions
    This function is, as its name suggests, strictly for debugging.
    I do not expect users to notice the existence of this function,
    let alone use this function in their programs.
    
    Also this patch implements several test cases based on it.

--- test/regression.py
+++ test/regression.py
@@ -2,21 +2,12 @@ import unittest
 import os
 import random
 import binascii
+from polyleven import _debug, levenshtein
 from itertools import product
-from Levenshtein import distance
-from polyleven import levenshtein
 
-class TestRandomPattern(unittest.TestCase):
-
-    def getstr(self):
-        r = os.urandom(random.randint(0, 512))
-        return binascii.hexlify(r).decode()
-
-    def test_simple(self):
-        for i in range(2048):
-            s1, s2 = self.getstr(), self.getstr()
-            with self.subTest(s1=s1, s2=s2):
-                self.assertEqual(distance(s1, s2), levenshtein(s1, s2))
+WAGNER_FISCHER = 0
+MBLEVEN = 1
+MYERS1999 = 2
 
 class TestEnumeratePatterns(unittest.TestCase):
 
@@ -31,7 +22,7 @@ class TestEnumeratePatterns(unittest.TestCase):
         dist, s1, s2 = line.strip().split('|')
         return int(dist), s1, s2
 
-    def test_basic(self):
+    def test_levenshtein(self):
         for line in self.fp:
             dist, s1, s2 = self.parseline(line)
 
@@ -39,12 +30,59 @@ class TestEnumeratePatterns(unittest.TestCase):
                 self.assertEqual(dist, levenshtein(s1, s2))
 
     def test_with_upperbound(self):
-        for k in range(5):
+        for k in (0, 1, 2, 3, 4, 5):
             for line in self.fp:
                 dist, s1, s2 = self.parseline(line)
 
                 with self.subTest(k=k,s1=s1,s2=s2):
                     self.assertEqual(min(dist, k+1), levenshtein(s1, s2, k))
+
+    def test_wagner_fischer(self):
+        for line in self.fp:
+            dist, s1, s2 = self.parseline(line)
+            with self.subTest(s1=s1, s2=s2):
+                self.assertEqual(dist, _debug(WAGNER_FISCHER, s1, s2))
+
+    def test_myers1999(self):
+        for line in self.fp:
+            dist, s1, s2 = self.parseline(line)
+            with self.subTest(s1=s1, s2=s2):
+                self.assertEqual(dist, _debug(MYERS1999, s1, s2))
+
+    def test_mbleven(self):
+        for k in (1, 2, 3):
+            for line in self.fp:
+                dist, s1, s2 = self.parseline(line)
+
+                with self.subTest(k=k,s1=s1,s2=s2):
+                    self.assertEqual(min(dist, k+1), _debug(MBLEVEN, s1, s2, k))
+
+
+class TestRandomPattern(unittest.TestCase):
+
+    def get_random_string(self, maxlen=512):
+        r = os.urandom(random.randint(0, maxlen))
+        return binascii.hexlify(r).decode()
+
+    def test_normal(self):
+        for i in range(1024):
+            s1 = self.get_random_string()
+            s2 = self.get_random_string()
+            with self.subTest(s1=s1, s2=s2):
+                self.assertEqual(
+                    _debug(WAGNER_FISCHER, s1, s2),
+                    _debug(MYERS1999, s1, s2)
+                )
+
+    def test_long_inputs(self):
+        for i in range(5):
+            s1 = self.get_random_string(4096)
+            s2 = self.get_random_string(4096)
+            with self.subTest(s1=s1, s2=s2):
+                self.assertEqual(
+                    _debug(WAGNER_FISCHER, s1, s2),
+                    _debug(MYERS1999, s1, s2)
+                )
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
