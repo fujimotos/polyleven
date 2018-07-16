7b08ce9186d31bbea9e371d526aa4b8309167eec
Fujimoto Seiji <fujimoto@ceptord.net>
2018-07-16 21:08:04 +0900

    setup: update package metadata
    This includes several improvements on packaging:
    
     1. Update the version of polyleven to 0.3
     2. Add Makefile.in to include LICENSE and README.md in the source
        distribution.
     3. Add long_description, keywords and license.

--- setup.py
+++ setup.py
@@ -2,16 +2,23 @@ import setuptools
 
 setuptools.setup(
     name='polyleven',
-    version='0.2',
+    version='0.3',
     author='Fujimoto Seiji',
     author_email='fujimoto@ceptord.net',
+    license='Public Domain',
     description='A fast C-implemented library for Levenshtein distance',
+    long_description="""\
+Polyleven is a C-implemented Python library that can compute Levenshtein
+distance between two strings. The focus of this library is efficiently.
+To archive this goal, polyleven combines a number of special case algorithms
+under the hood.""",
     url='https://github.com/fujimotos/polyleven',
     ext_modules=[
         setuptools.Extension('polyleven', sources=['polyleven.c'])
     ],
     zip_safe=False,
     python_requires='>=3.4',
+    keywords=['Levenshtein distance'],
     classifiers=(
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
