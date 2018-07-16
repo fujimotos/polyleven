dad47f51dd77a1d02a6b7a0032ddfb7bf0ea22a2
Fujimoto Seiji <fujimoto@ceptord.net>
2018-07-16 20:36:39 +0900

    Add test cases for non-ascii characters
    This ensures that polyleven works with inputs that contain multibyte
    characters i.e. those out of the range of [\u0000...\u00ff].
    
    For this purpose, I use Japanese Hiragana, Katakana and CJK symbols
    that reside in the range of [\u3000...\u30ff].

--- test/regression.py
+++ test/regression.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import random
 import binascii
+import string
 from polyleven import _debug, levenshtein
 from itertools import product
 
@@ -60,24 +61,64 @@ class TestEnumeratePatterns(unittest.TestCase):
 
 class TestRandomPattern(unittest.TestCase):
 
-    def get_random_string(self, maxlen=512):
-        r = os.urandom(random.randint(0, maxlen))
-        return binascii.hexlify(r).decode()
+    def get_random_string(self, minlen, maxlen):
+        n = random.randint(minlen, maxlen)
+        b = os.urandom(n)
+        return ''.join(string.printable[i & 0x3f] for i in b)
 
-    def test_normal(self):
-        for i in range(1024):
-            s1 = self.get_random_string()
-            s2 = self.get_random_string()
+    def test_short(self):
+        for i in range(2000):
+            s1 = self.get_random_string(0, 16)
+            s2 = self.get_random_string(0, 16)
             with self.subTest(s1=s1, s2=s2):
                 self.assertEqual(
                     _debug(WAGNER_FISCHER, s1, s2),
                     _debug(MYERS1999, s1, s2)
                 )
 
-    def test_long_inputs(self):
-        for i in range(5):
-            s1 = self.get_random_string(4096)
-            s2 = self.get_random_string(4096)
+    def test_medium(self):
+        for i in range(1000):
+            s1 = self.get_random_string(128, 256)
+            s2 = self.get_random_string(128, 256)
+            with self.subTest(s1=s1, s2=s2):
+                self.assertEqual(
+                    _debug(WAGNER_FISCHER, s1, s2),
+                    _debug(MYERS1999, s1, s2)
+                )
+
+    def test_long(self):
+        for i in range(100):
+            s1 = self.get_random_string(512, 1024)
+            s2 = self.get_random_string(512, 1024)
+            with self.subTest(s1=s1, s2=s2):
+                self.assertEqual(
+                    _debug(WAGNER_FISCHER, s1, s2),
+                    _debug(MYERS1999, s1, s2)
+                )
+
+class TestUnicode(unittest.TestCase):
+
+    KANAHIRA = "".join(chr(i) for i in range(0x3000, 0x3100))
+
+    def get_random_string(self, minlen, maxlen):
+        n = random.randint(minlen, maxlen)
+        b = os.urandom(n)
+        return ''.join(self.KANAHIRA[i & 0xff] for i in b)
+
+    def test_unicode_short(self):
+        for i in range(2000):
+            s1 = self.get_random_string(0, 16)
+            s2 = self.get_random_string(0, 16)
+            with self.subTest(s1=s1, s2=s2):
+                self.assertEqual(
+                    _debug(WAGNER_FISCHER, s1, s2),
+                    _debug(MYERS1999, s1, s2)
+                )
+
+    def test_unicode_medium(self):
+        for i in range(1000):
+            s1 = self.get_random_string(128, 256)
+            s2 = self.get_random_string(128, 256)
             with self.subTest(s1=s1, s2=s2):
                 self.assertEqual(
                     _debug(WAGNER_FISCHER, s1, s2),
