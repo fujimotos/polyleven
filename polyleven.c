f10260ba3ec449ec77b0f8dc91131da490e62a85
Fujimoto Seiji <fujimoto@ceptord.net>
2019-08-17 21:08:32 +0900

    Remove the implementation of the Wagner-Fischer algorithm.
    I noticed that myers1999 (if implemented properly) always beats
    the Wagner-Fischer algorithm in speed. Look here:
    
    Before:
    
        #                              TIME[sec]       SPEED[calls/s]
        polyleven.levenshtein          0.359           2759863
    
    After:
    
        #                              TIME[sec]       SPEED[calls/s]
        polyleven.levenshtein          0.354           2804051
    
    So the speed impact of removing Wagner-Fischer is really negligible
    (or non-existent at all). Let's just simplify the code here.
    
    Signed-off-by: Fujimoto Seiji <fujimoto@ceptord.net>

--- polyleven.c
+++ polyleven.c
@@ -29,16 +29,6 @@ static void strbuf_init(PyObject *unicode, struct strbuf *sb)
 
 #define STRBUF_READ(sb,idx) (PyUnicode_READ((sb)->kind, (sb)->data, (idx)))
 
-static Py_ssize_t strbuf_find(struct strbuf *sb, Py_UCS4 chr, Py_ssize_t start)
-{
-    Py_ssize_t idx;
-    for (idx = start; idx < sb->len; idx++) {
-        if (STRBUF_READ(sb, idx) == chr)
-            return idx;
-    }
-    return -1;
-}
-
 /*
  * The model table for mbleven algorithm.
  */
@@ -119,144 +109,6 @@ static Py_ssize_t mbleven(struct strbuf *sb1, struct strbuf *sb2, Py_ssize_t k)
 
     return res;
 }
-
-/*
- * WF1: Optimized for where sb2->len == 1;
- */
-static Py_ssize_t wagner_fischer_L1(struct strbuf *sb1, struct strbuf *sb2)
-{
-    Py_UCS4 c0 = STRBUF_READ(sb2, 0);
-    Py_ssize_t i0 = strbuf_find(sb1, c0, 0);
-    return sb1->len - (i0 > -1);
-}
-
-/*
- * WF2: Optimized for where sb2->len == 2;
- */
-static Py_ssize_t wagner_fischer_L2(struct strbuf *sb1, struct strbuf *sb2)
-{
-    Py_UCS4 c0, c1;
-    Py_ssize_t i0, i1;
-
-    c0 = STRBUF_READ(sb2, 0);
-    c1 = STRBUF_READ(sb2, 1);
-
-    i0 = strbuf_find(sb1, c0, 0);
-
-    if (i0 == -1 || i0 == sb1->len - 1) {
-        i1 = strbuf_find(sb1, c1, 1);
-        return sb1->len - (i1 > -1);
-    } else {
-        i1 = strbuf_find(sb1, c1, i0 + 1);
-        return sb1->len - (i1 > -1) - 1;
-    }
-}
-
-/*
- * An optimized implementation of Wagner-Fischer.
- *
- * The basic idea behind this routine is to avoid filling cells which
- * never produces a sensible edit path; For example, if sb1='abcd' and
- * sb2='xyz', it just computes following cells:
- *
- *        x y z
- *      0 1
- *    a 1 1 2
- *    b 2 2 2 3
- *    c   3 3 3
- *    d     4 4
- *
- * This provides about a 20% speedup on our benchmark.
- *
- * Note that this optimization does not work when sb2->len <= 2.
- * Use wagner_fischer_L* instead for such cases.
- */
-static Py_ssize_t wagner_fischer_with_cutoff(struct strbuf *sb1, struct strbuf *sb2, Py_ssize_t *arr)
-{
-    Py_ssize_t i, j, rpad, lpad;
-    Py_ssize_t start, end, top, left, dia;
-    Py_UCS4 chr;
-
-    rpad = (sb2->len - 1) / 2;
-    lpad = rpad + (sb1->len - sb2->len);
-
-    for (j = 0; j <= rpad; j++)
-        arr[j] = j;
-
-    for (i = 1; i <= sb1->len; i++) {
-        arr[0] = i - 1;
-        chr = STRBUF_READ(sb1, i - 1);
-
-        start = MAX(1, i - lpad);
-        dia = arr[start - 1];
-        top = arr[start];
-
-        if (chr != STRBUF_READ(sb2, start - 1)) {
-            dia = MIN(dia, top);
-            dia++;
-        }
-        arr[start] = dia;
-        left = dia;
-        dia = top;
-
-        /*
-         * Process cells where both the cell above and to the
-         * left is filled.
-         */
-        end = i + rpad - 1;
-        if (sb2->len < i + rpad)
-            end = sb2->len;
-
-        for (j = start + 1; j <= end; j++) {
-            top = arr[j];
-
-            if (chr != STRBUF_READ(sb2, j - 1)) {
-                dia = MIN(dia, top);
-                dia = MIN(dia, left);
-                dia++;
-            }
-            arr[j] = dia;
-            left = dia;
-            dia = top;
-        }
-
-        if (sb2->len < i + rpad)
-            continue;
-
-        if (chr != STRBUF_READ(sb2, end)) {
-            dia = MIN(dia, left);
-            dia++;
-        }
-        arr[end + 1] = dia;
-    }
-    dia = arr[sb2->len];
-    return dia;
-}
-
-static Py_ssize_t wagner_fischer(struct strbuf *sb1, struct strbuf *sb2)
-{
-    Py_ssize_t *arr;
-    Py_ssize_t res;
-
-    if (!sb2->len)
-        return sb1->len;
-    if (sb2->len == 1)
-        return wagner_fischer_L1(sb1, sb2);
-    if (sb2->len == 2)
-        return wagner_fischer_L2(sb1, sb2);
-
-    arr = malloc((sb2->len + 1) * sizeof(Py_ssize_t));
-    if (arr == NULL) {
-        PyErr_NoMemory();
-        return -1;
-    }
-
-    res = wagner_fischer_with_cutoff(sb1, sb2, arr);
-    free(arr);
-
-    return res;
-}
-
 
 /*
  * Myers' bit-parallel algorithm
@@ -456,10 +308,8 @@ static PyObject* polyleven_levenshtein(PyObject *self, PyObject *args)
         res = PyUnicode_Compare(u1, u2) ? 1 : 0;
     } else if (1 <= k && k <= 3) {
         res = mbleven(&sb1, &sb2, k);
-    } else if (3 <= sb2.len) {
-        res = myers1999(&sb1, &sb2);
     } else {
-        res = wagner_fischer(&sb1, &sb2);
+        res = myers1999(&sb1, &sb2);
     }
 
     if (res < 0)
